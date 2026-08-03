3e476893db4095d76f1dc2593e31d29ebbfbf3ad
Ceris1107 <ila413546@gmail.com>
2026-08-03 15:55:55 +0300

    Create update_license_year.py

---
+++ scripts/update_license_year.py
@@ -0,0 +1,11 @@
+import re
+from datetime import datetime
+
+with open("LICENSE.md", "r+") as f:
+    c = f.read()
+    y = sorted(map(int, re.findall(r'\b20\d{2}\b', c)))
+    if y:
+        n = f"{min(y)}-{datetime.now().year}" if datetime.now().year != min(y) else str(min(y))
+        f.seek(0)
+        f.write(re.sub(r'\b20\d{2}\b', n, c))
+        f.truncate()
